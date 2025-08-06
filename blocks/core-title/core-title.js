33cb4911a0055ee06855bf7f1943434bc018473e
mathlov3 <vzrivpaket22@gmail.com>
2025-08-06 23:31:21 +0300

    Feature 1 core title (#2)
    #1 Core Title: Implement TItle component from AEM Core library

---
+++ blocks/core-title/core-title.js
@@ -0,0 +1,63 @@
+import { getSimpleRowContent, StringUtils } from '../../scripts/utils.js';
+
+const AVAILABLE_HEADER_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+
+const getLinkFromRow = (row) => {
+  const link = row.querySelector('a');
+  if (!link) {
+    return null;
+  }
+  return link.getAttribute('href');
+};
+
+const isLinkValid = (link) => !!link;
+
+const getRootRag = () => {
+  const titleRootTag = document.createElement('div');
+  titleRootTag.classList.add('cmp-title');
+  return titleRootTag;
+};
+
+const getHeaderElement = (tagName = '') => {
+  let tag = tagName.toLowerCase();
+  if (!AVAILABLE_HEADER_TAGS.includes(tagName)) {
+    tag = 'h1';
+  }
+  const header = document.createElement(tag);
+  header.classList.add('cmp-title__text');
+  return header;
+};
+
+const getLinkElement = (link, target, text, id) => {
+  const linkElement = document.createElement('a');
+  linkElement.classList.add('cmp-title__link');
+  linkElement.setAttribute('href', link);
+  linkElement.setAttribute('target', target);
+  linkElement.textContent = text;
+  if (id && !!id.trim()) {
+    linkElement.setAttribute('id', id.trim());
+  }
+  return linkElement;
+};
+
+export default function decorate(block) {
+  const rows = [...block.children];
+  const text = StringUtils.trimToEmpty(getSimpleRowContent(rows[0]));
+  const headerTag = StringUtils.trimToEmpty(getSimpleRowContent(rows[1]));
+  const link = getLinkFromRow(rows[2]);
+  const target = StringUtils.trimToEmpty(getSimpleRowContent(rows[3])) === 'true' ? '_blank' : '_self';
+  const id = StringUtils.trimToEmpty(getSimpleRowContent(rows[4]));
+
+  const root = getRootRag();
+  const header = getHeaderElement(headerTag);
+  root.append(header);
+  if (isLinkValid(link)) {
+    header.append(getLinkElement(link, target, text, id));
+  } else {
+    header.textContent = text;
+  }
+
+  block.innerHTML = '';
+  block.append(root);
+  block.classList.add('title');
+}
